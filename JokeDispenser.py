530143f0bcc7c049448f55a5d7cdcdeb87470fab
Hitengupta-coding <coolguyyy88@gmail.com>
2025-04-20 17:11:18 +0530

    HexConvertor.py

--- JokeDispenser.py
+++ JokeDispenser.py
@@ -1,17 +1,48 @@
-def jokes():
-    import pyjokes
-    permit = input("Want some jokes? (yes/no): ").lower()
-    if permit == "yes":
-        joke = pyjokes.get_joke(language='en',category='neutral')
-        print("\n")
-        print(joke)
-        print("\n")
-        jokes()
-    elif permit == "no":
-        print("Ok")
+# By Hitengupta-coding
+import binascii
+print('This is a text to hex and hex to text convertor.')
+def tryagain():
+    qt = input('Do you want to use more(y/n): ')
+
+    if qt=="y":
+        convert()
+
+    elif qt=="n":
+        print('Thanks for using our software, Goodbye')
+
     else:
-        print("\n")
-        print('Wrong Statement')
-        print("\n")
-        jokes()
-jokes()
+        print('You entered invalid statement, try again')
+        tryagain()
+
+def convert():
+    def thmode():
+        text = input('Enter the text to be converted: ')
+        hex_output = binascii.hexlify(text.encode()).decode()
+        print(hex_output)
+
+    def htmode():
+        try:
+            hex_string = input('Enter the hex to be converted: ')
+            text = binascii.unhexlify(hex_string).decode()
+            print(text)
+        except:
+            print('your hex is wrong!')
+            htmode()
+
+
+    def ask():
+        mode = input("Enter 'TH' to convert text to hex OR Enter 'HT' to convert hex to text: ").lower()
+        if mode == "th":
+            thmode()
+
+        elif mode == "ht":
+            htmode()
+
+        else:
+            print('wrong statement, try again.')
+            ask()
+
+    ask()
+    tryagain()
+
+convert()
